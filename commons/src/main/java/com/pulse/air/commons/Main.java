b1527c6e681e6aaaf9d302b43dd184943a34794b
Haris Hindić <56647882+haris-hindic@users.noreply.github.com>
2023-09-29 23:44:28 +0200

    Initial commit

---
+++ commons/src/main/java/com/pulse/air/commons/Main.java
@@ -0,0 +1,7 @@
+package com.pulse.air.commons;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ flight-catalogue-service/src/main/java/com/pulse/air/flightcatalogue/FlightCatalogueLauncher.java
@@ -0,0 +1,12 @@
+package com.pulse.air.flightcatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightCatalogueLauncher {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(FlightCatalogueLauncher.class, args);
+	}
+}
